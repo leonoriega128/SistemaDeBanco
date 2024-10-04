a42591c458f61ab3b7b5a1760b5199a28a924c13
lnoriegaexc <106536890+lnoriegaexc@users.noreply.github.com>
2024-10-03 23:24:15 -0300

    envio de cambios a repos original (#5)
    * menu para diferentes proyectos
    
    * ultimos cambios del taller 1
    
    ---------
    
    Co-authored-by: lnoriega <leonoriega128@gmail.com>

--- src/main/java/com/sistemabancario/ServidorTCP.java
+++ src/main/java/com/sistemabancario/ServidorTCP.java
@@ -9,6 +9,9 @@ import java.net.Socket;
 public class ServidorTCP {
 
     public static void main(String[] args) {
+        //agregar hilo para detector de fallas, sería una clase nueva por agregar,
+        // la clase debería ser un thread, debe tener una variable que indique cual 
+        // es el servidor que esta funcionando
         try (ServerSocket server = new ServerSocket(7777)) {
             while (true) {
                 Socket sock = server.accept();
@@ -17,9 +20,10 @@ public class ServidorTCP {
                 // Crear los streams
                 DataInputStream in = new DataInputStream(sock.getInputStream());
                 DataOutputStream out = new DataOutputStream(sock.getOutputStream());
-
+                 String nombreCliente = in.readUTF();
+                System.out.println("Nombre del cliente: " + nombreCliente);
                 // Crear y empezar el hilo, pasando el socket
-                ServidorHilo hilo = new ServidorHilo(sock, in, out, ""); // Inicialmente con nombre vacío
+                ServidorHilo hilo = new ServidorHilo(sock, in, out,nombreCliente); // Inicialmente con nombre vacío
                 hilo.start();
             }
         } catch (IOException e) {
